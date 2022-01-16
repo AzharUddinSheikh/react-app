fd645bc7b458e756fa71241cce4a858c22716642
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2022-01-17 00:50:19 +0530

    conditional redering on a link

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count:0,
+        count:2,
         name:'azhar',
-        imgUrl:'https://picsum.photos/200'
+        imgUrl:'https://picsum.photos/200',
+        tag: ['tag1', 'tag2','tag3']
     };
 
     styles = {
@@ -12,12 +13,21 @@ class Counter extends Component {
         fontWeight: 'bold'
     }
 
+    renderTags() {
+        if (this.state.count ===0) return <p>There are no tags</p>
+        return <ul>{ this.state.tag.map(tag=> <li key={tag}>{tag}</li> )}</ul>;
+    }
+
     render() { 
         return <React.Fragment>
             <h1 style={{ fontSize:30 }}>Hello {this.state.name}</h1><br/>
             <img src={this.state.imgUrl} alt="random-pic" />
             <span className={ this.getBadgeClass() } style={this.styles}>Count is {this.formatCount()}</span>
-            <button className='badge btn-primary m-2'>Increment</button>
+            <button className='badge btn-primary m-2'>Increment</button><br />
+            <div>
+                {this.state.count ===0 && "please create tag!"}
+                {this.renderTags()}
+            </div>
         </React.Fragment>;
     }
 
