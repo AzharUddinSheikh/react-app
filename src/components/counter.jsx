68192019d54a209169b0538f158711f565a1ee46
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2022-01-17 00:33:29 +0530

    dynamically calling classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count:1,
+        count:0,
         name:'azhar',
         imgUrl:'https://picsum.photos/200'
     };
@@ -16,10 +16,17 @@ class Counter extends Component {
         return <React.Fragment>
             <h1 style={{ fontSize:30 }}>Hello {this.state.name}</h1><br/>
             <img src={this.state.imgUrl} alt="random-pic" />
-            <span style={this.styles}>Count is {this.formatCount()}</span>
+            <span className={ this.getBadgeClass() } style={this.styles}>Count is {this.formatCount()}</span>
             <button className='badge btn-primary m-2'>Increment</button>
         </React.Fragment>;
     }
+
+    getBadgeClass() {
+        let classes = "badge m-2 btn-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
     formatCount() {
         const { count } = this.state;
         return count === 0 ? 'Zero' : count;
