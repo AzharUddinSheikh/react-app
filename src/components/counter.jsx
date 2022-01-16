86d1faeb9c9f7d1ee83adb93b27db23cef457f0e
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2022-01-16 12:54:08 +0530

    embedding expression using state of a class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+        count:1,
+        name:'azhar'
+    };
     render() { 
         return <React.Fragment>
-            <h1>Hello World</h1><button>Increment</button>
+            <h1>Hello {this.state.name}</h1><br/>
+            <span>Count is {this.formatCount()}</span>
+            <button>Increment</button>
         </React.Fragment>;
+    }
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
  
