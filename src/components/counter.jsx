ec5b5bc8ca1ea93e0e1eebc04d6abafa936ef16a
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2022-01-17 00:22:24 +0530

    setting atrribute like image and inline styles and via object

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,21 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count:1,
-        name:'azhar'
+        name:'azhar',
+        imgUrl:'https://picsum.photos/200'
     };
+
+    styles = {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+
     render() { 
         return <React.Fragment>
-            <h1>Hello {this.state.name}</h1><br/>
-            <span>Count is {this.formatCount()}</span>
-            <button>Increment</button>
+            <h1 style={{ fontSize:30 }}>Hello {this.state.name}</h1><br/>
+            <img src={this.state.imgUrl} alt="random-pic" />
+            <span style={this.styles}>Count is {this.formatCount()}</span>
+            <button className='badge btn-primary m-2'>Increment</button>
         </React.Fragment>;
     }
     formatCount() {
